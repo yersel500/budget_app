f281a7706e4ae5fab958f4534564c23425b50faf
yersel500 <yersel500@gmail.com>
2022-06-24 19:42:02 -0500

    Add CRUD operations for categories controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,26 @@
 class CategoriesController < ApplicationController
+  def index
+    @user = User.find(params[:user_id])
+    @categories = @user.categories
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @category = @user.categories.new(category_params)
+    if @category.save
+      flash[:notice] = 'The category was added successfully'
+      redirect_to user_categories_path(user_id: params[:user_id])
+    else
+      flash[:alert] = "Couldn't create the recipe!"
+      render :new
+    end
+  end
+
+  def category_params
+    params.require(:category).permit(:name, :icon)
+  end
 end
