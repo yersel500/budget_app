e268c981e39571835d50ba65ba2a48780fc5cc87
yersel500 <yersel500@gmail.com>
2022-06-29 14:54:40 -0500

    delete duplicated code

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
   has_many :categories, dependent: :destroy
   has_many :operations, dependent: :destroy
 
