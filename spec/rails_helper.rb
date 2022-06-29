d90585c31de6f4f70a698df7f6cb2ad86c7a2cec
yersel500 <yersel500@gmail.com>
2022-06-29 14:55:51 -0500

    Testing for categories

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,8 +3,11 @@ require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 # Prevent database truncation if the environment is production
-abort("The Rails environment is running in production mode!") if Rails.env.production?
+abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
+require 'capybara'
+# Add additional requires below this line. Rails is not loaded until this point!
+require 'support/database_cleaner'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -37,7 +40,7 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
 
   # You can uncomment this line to turn off ActiveRecord support entirely.
   # config.use_active_record = false
---
+++ spec/system/categories_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+RSpec.describe 'FoodPage', type: :system do
+  before :all do
+    @user = User.create!(full_name: 'Babi',
+                         email: 'b@g.com', password: '123456',
+                         created_at: '2022-06-15 06:29:35.011315')
+
+    Category.create(id: 110, user: User.first, name: 'school', icon: 'https://cdn-prod.voxy.com/wp-content/uploads/2012/10/school-1.jpg')
+    Category.create(user: User.first, name: 'health', icon: 'https://cdn-prod.voxy.com/wp-content/uploads/2012/10/school-1.jpg')
+    Category.create(user: User.first, name: 'loan', icon: 'https://cdn-prod.voxy.com/wp-content/uploads/2012/10/school-1.jpg')
+    Category.create(user: User.first, name: 'Electricity', icon: 'https://cdn-prod.voxy.com/wp-content/uploads/2012/10/school-1.jpg')
+  end
+
+  context 'testing' do
+    before :each do
+      visit root_path
+      fill_in 'Email', with: 'b@g.com'
+      fill_in 'Password', with: '123456'
+      click_button 'Log in'
+    end
+
+    it 'should contain "school"' do
+      expect(page).to have_content('school')
+    end
+
+    it 'the categories items count should be #4' do
+      count = all('.my-icon').count
+      expect(count).to eq 4
+    end
+
+    it 'should navigate to the category form page' do
+      click_link 'Create New Category'
+      expect(page).to have_current_path(new_user_category_path(user_id: @user.id))
+    end
+  end
+end
